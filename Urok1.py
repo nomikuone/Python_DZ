77d07ebdb8fb8b3c1b3893395828d20372933a08
Anzhela <nomiku@mail.ru>
2022-10-14 21:51:45 +0300

    two commit

--- Urok1.py
+++ Urok1.py
@@ -24,3 +24,26 @@ def checkNumber(num):
 
 num = InputNumbers("Введите число: ")
 checkNumber(num)
+
+# Напишите программу для. проверки истинности утверждения 
+# \¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
