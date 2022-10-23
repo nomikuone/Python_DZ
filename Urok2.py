85ac68aae07961666e0ceabe2c6abb617411f101
nomikuone <111079701+nomikuone@users.noreply.github.com>
2022-10-23 23:42:48 +0300

    Update Urok2.py
    commit

--- Urok2.py
+++ Urok2.py
@@ -52,3 +52,36 @@ for e in range(1, num + 1):
     list.append(mult(e))
 
 print(f"Произведения чисел от 1 до {num}:  {list}")
+
+# Задача 3. Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму
+
+n = int(input("Введите число n: "))
+s = 0
+for i in range(1,n+1):
+    s += (1+1/i)**i
+print(f"Полученная сумма последовательности (1+1/n)^n равна {round(s,2)}")
+
+
+# Задача 4. Задать список из N элементов, заполненных числами из [-N, N]. 
+# Найти произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число
+
+import random
+def write_file(number):
+    with open('file.txt', 'w') as data:
+        for i in range(number):
+            data.write(f"{random.randrange(0, 2*number)}\n")
+
+
+def read_file():
+    with open('file.txt', 'r') as data:
+        indexs = list(map(int,data.readlines()))
+    return indexs
+
+# Задача 5. Реализовать алгоритм перемешивания списка.
+
+import random
+lst = [random.randint(0,10) for i in range(random.randint(5,20))]
+print(f"исходный список:\n {lst}")
+random.shuffle(lst)
+print(f"список после перемешивания:\n{lst}")
